988319a86ee692de0323cbef5a07dae25a42e17e
Andrei <kirylchyk.andrei@gmail.com>
2025-08-29 14:55:47 +0300

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -1,22 +1,16 @@
-function openSettingsDialog() {
+function openSettingsDialog(event) {
     Office.context.ui.displayDialogAsync(
-        "https://kirryya.github.io/addIn/taskpane.html", // URL вашего HTML
-        { height: 50, width: 50, displayInIframe: true }, // можно регулировать размер
-        function (asyncResult) {
-            if (asyncResult.status === Office.AsyncResultStatus.Failed) {
-                console.error(asyncResult.error.message);
-            } else {
-                // dialog получаем через asyncResult.value
-                const dialog = asyncResult.value;
-
-                // Можно слушать сообщение из диалога
-                dialog.addEventHandler(Office.EventType.DialogMessageReceived, (arg) => {
-                    console.log("Message from dialog:", arg.message);
-                    dialog.close(); // закрыть диалог при необходимости
-                });
-            }
-        }
+        "https://kirryya.github.io/addIn/taskpane.html",
+        { height: 50, width: 50, displayInIframe: true }
     );
+
+    // Обязательно уведомляем Excel, что функция завершена
+    if (event) {
+        event.completed();
+    }
 }
 
-window.openSettingsDialog = openSettingsDialog;
+// Делаем глобальной
+if (typeof window !== "undefined") {
+    window.openSettingsDialog = openSettingsDialog;
+}
