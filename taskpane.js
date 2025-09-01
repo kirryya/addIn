293c9d5c997ee4d8cfcaf3ffb78fcbd141e8cd94
Andrei <kirylchyk.andrei@gmail.com>
2025-09-01 14:59:52 +0300

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -1,11 +1,5 @@
 function openSettingsDialog() {
-    console.log("Кнопка 'Новый шаблон' нажата!");
-    Office.context.ui.displayDialogAsync(
-        "https://kirryya.github.io/addIn/dailog.html",
-        { height: 40, width: 60, displayInIframe: false }
-    );
+    Office.context.ui.displayDialogAsync("https://kirryya.github.io/addIn/dailog.html");
 }
 
 Office.actions.associate("openSettingsDialog", openSettingsDialog);
-
-console.log("Taskpane.js загружен")
