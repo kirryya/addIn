c25994bc06989fd05895698d1eb387584f3529a7
Andrei <kirylchyk.andrei@gmail.com>
2025-09-01 17:10:50 +0300

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -1,12 +1,23 @@
 Office.onReady(() => {
-    // Привязываем идентификатор insertSheets к функции
-    Office.actions.associate("insertSheets", insertSheets);
+    Office.actions.associate("generalSettings", generalSettings);
+    Office.actions.associate("newTemplate", newTemplate);
 });
 
-function insertSheets(event) {
+function generalSettings(event) {
+    Office.context.ui.displayDialogAsync(
+        "https://kirryya.github.io/addIn/taskpane.html",
+        { height: 40, width: 60, displayInIframe: true }
+    );
+
+    if (event && typeof event.completed === "function") {
+        event.completed();
+    }
+}
+
+function newTemplate(event) {
     Office.context.ui.displayDialogAsync(
         "https://kirryya.github.io/addIn/dailog.html",
-        { height: 60, width: 40, displayInIframe: true }
+        { height: 40, width: 60, displayInIframe: true }
     );
 
     if (event && typeof event.completed === "function") {
