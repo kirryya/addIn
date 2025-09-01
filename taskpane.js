6f527483ee30810b8c0309e713e172b77abecaa9
Andrei <kirylchyk.andrei@gmail.com>
2025-09-01 15:39:00 +0300

    Update taskpane.js

--- taskpane.js
+++ taskpane.js
@@ -1,5 +1,12 @@
 function openSettingsDialog() {
-    Office.context.ui.displayDialogAsync("https://kirryya.github.io/addIn/dailog.html");
+    console.log("openSettingsDialog called");
+    Office.context.ui.displayDialogAsync("https://kirryya.github.io/addIn/dailog.html", function (result) {
+        if (result.status === Office.AsyncResultStatus.Failed) {
+            console.error("Failed to open dialog: ", result.error);
+        } else {
+            console.log("Dialog opened successfully");
+        }
+    });
 }
 
 Office.actions.associate("openSettingsDialog", openSettingsDialog);
